5bb324de1b02ae8e26ba9c80e3ad6b4007c7d4aa
Shadowmeth <47725754+Shadowmeth@users.noreply.github.com>
2020-09-25 22:20:05 +0500

    Update largest_of_3_numbers.java

--- largest_of_3_numbers.java
+++ largest_of_3_numbers.java
@@ -1,1 +1,26 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Q6 {
+
+	public static void main(String[] args) {
+		// take 3 numbers input from user and output the largest number
+		Scanner input = new Scanner(System.in);
+		int n1, n2, n3;
+		n1 = input.nextInt();
+		n2 = input.nextInt();
+		n3 = input.nextInt();
+	
+		if (n1 > n2 && n1 > n3) {
+			System.out.println(n1 + " is largest");
+		} else if (n2 > n1 && n2 > n3) {
+			System.out.println(n2 + " is largest");
+		} else if (n3 > n1 && n3 > n2) {
+			System.out.println(n3 + " is largest");
+		}
+		
+	}
+
+}
 
